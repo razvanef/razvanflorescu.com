b4ddbfb7c9e456a09b40a7cd14742103a039ec50
Razvan Florescu <razvanef@gmail.com>
2023-01-28 18:27:58 +0200

    front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { SocialLinks } from '../components/SocialLinks'
 
 const Home: NextPage = () => {
     return (
-        <div className="p-12 w-2/5 md:w-100 mx-auto">
+        <div className="p-12 lg:w-2/5 md:w-2/5 sm:w-2/4 w-full mx-auto">
             <div className="logo text-4xl mb-10">
                 razvan florescu
             </div>
@@ -13,11 +13,10 @@ const Home: NextPage = () => {
                 Currently building <a href="https://www.druidai.com/chatbots-in-the-press/druid-launches-a-new-conversational-ai-category-conversational-business-applications" data-toggle="tooltip" data-placement="top" title="Conversational Business Applications" target="_blank" rel="noreferrer">CBA</a> at <a href="https://www.druidai.com/chatbots-in-the-press/" target="_blank" rel="noreferrer">Druid AI</a> platform and <a href="https://esennoua.ro" target="_blank" rel="noreferrer">Esential Noua</a>, a sustenability zero-waste brand.<br />
                 Born on a tuesday afternoon in Ploiesti, Romania, living and studying in Bucharest.
             </p>
-            <br />
-            <p>
-                Go check my <Link href="/notes">/notes</Link> and <Link href="/bookmarks">/bookmarks</Link>.
-            </p>
-            <SocialLinks />
+            <div className="flex justify-between items-center text-stone-500 mt-6">
+                <div><Link href="/notes">/notes</Link> <Link href="/bookmarks">/bookmarks</Link></div><SocialLinks />
+            </div>
+            
         </div>
     )
 }
