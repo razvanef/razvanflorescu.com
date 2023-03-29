7928dde363774011d9c0b3a05f452cde7b6ee00d
Razvan Florescu <razvanef@gmail.com>
2023-03-29 16:43:03 +0300

    add dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.scss'
 import '../node_modules/line-awesome/dist/line-awesome/css/line-awesome.min.css'
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
+import { DarkModeToggle } from '../components/DarkModeToggle'
+import { ThemeContext } from '../context/ThemeContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
     const [theme, setTheme] = useState((typeof window !== "undefined" && window.localStorage.getItem('theme')) || "light-mode")
